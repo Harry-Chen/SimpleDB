69925d093ec241ab88b0408ec7dce7845be30888
Harry Chen <i@harrychen.xyz>
2017-11-20 21:18:04 +0800

    Remove unused file
    Signed-off-by: Harry Chen <i@harrychen.xyz>

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,15 @@
-#include <fstream>
-#include <iostream>
-
-#include "sql/execute.h"
-#include "record/DB.h"
-#include "sql/parser.h"
 #include "dbms/dbms.h"
 
-int main(int argc, char const *argv[])
-{
-  assert(sizeof(TableHead) <= PAGE_SIZE);
-  if(argc < 2){
-    return start_parse(NULL); //read SQL from STDIN
-  }else{
-    DBMS::getInstance()->switchToDB(argv[1]); //first parameter is DB name
-    return start_parse(NULL);
-  }
-  return 0;
+#ifdef __cplusplus
+extern "C" char start_parse(const char *expr_input);
+#endif
+
+int main(int argc, char const *argv[]) {
+    //assert(sizeof(TableHead) <= PAGE_SIZE);
+    if (argc < 2) {
+        return start_parse(nullptr); //read SQL from STDIN
+    } else {
+        DBMS::getInstance()->switchToDB(argv[1]); //first parameter is DB name
+        return start_parse(nullptr);
+    }
 }
